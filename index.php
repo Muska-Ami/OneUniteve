29050d580d0fac94a067e588fedbdaeeb0bf3a90
XiaMoHuaHuo-CN <2549619802@qq.com>
2022-07-14 16:24:20 +0800

    未完成

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 if(isset($_GET['reader'])) {
     switch ($_GET['reader']) {
         case 'api':
@@ -8,26 +9,14 @@ if(isset($_GET['reader'])) {
             $path = $_GET['path'];
             if (isset($path)) {
                 $pathd = "source/" . $path;
-                if(!file_exists($pathd)) {
-                    http_response_code(404);
-                } else {
-                    switch ($_GET['type']) {
-                        case 'css':
-                            header("Content-Type: text/css");
-                            $data = file_get_contents($pathd);
-                            echo $data;
-                            break;
-                        case 'js':
-                            header("Content-Type: text/javascript");
-                            $data = file_get_contents($pathd);
-                            echo $data;
-                            break;
-                        case null:
-                            $data = file_get_contents($pathd);
-                            echo $data;
-                            break;
-                    }
-                }
+                extracted($pathd);
+            }
+            break;
+        case 'theme-resource':
+            $path = $_GET['path'];
+            if (isset($path)) {
+                $pathd = "theme/" . $path;
+                extracted($pathd);
             }
             break;
     }
@@ -47,4 +36,28 @@ if(isset($_GET['reader'])) {
         file_get_contents("theme/" . $theme . "/main.html")
     );
     echo $html;
+}
+
+function extracted(string $pathd): void
+{
+    if (!file_exists($pathd)) {
+        http_response_code(404);
+    } else {
+        switch ($_GET['type']) {
+            case 'css':
+                header("Content-Type: text/css");
+                $data = file_get_contents($pathd);
+                echo $data;
+                break;
+            case 'js':
+                header("Content-Type: text/javascript");
+                $data = file_get_contents($pathd);
+                echo $data;
+                break;
+            case null:
+                $data = file_get_contents($pathd);
+                echo $data;
+                break;
+        }
+    }
 }
--- source/js/login.js
+++ source/js/login.js
@@ -6,8 +6,6 @@ function login() {
     username = md5(username.value);
     password = sha224(sha256(md5(password.value)));
 
-    const token = grecaptcha.getResponse();
-
     if (username === 'd41d8cd98f00b204e9800998ecf8427e') {
         tipcontainer.style.color = 'red';
         tipcontainer.innerHTML = '请填入用户名';
@@ -17,7 +15,6 @@ function login() {
     } else {
         tipcontainer.style.color = '';
         tipcontainer.innerHTML = '';
-        window.location = '../../api/auth/session/login?username=' + username + '&password=' + password + '&captcha_token=' + token;
-        //console.log('auth/session/login?username=' + username + '&password=' + password + '&captcha_token=' + token);
+        window.location = '../../api/auth/session/login?username=' + username + '&password=' + password;
     }
 }
