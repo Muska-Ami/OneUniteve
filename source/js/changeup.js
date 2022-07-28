7b8e2bd1d81b7f54e228ab5ed8a402ffc26606cd
XiaMoHuaHuo_CN <79651409+XiaMoHuaHuo-CN@users.noreply.github.com>
2022-07-28 09:30:54 +0800

    Delete source directory

--- source/js/changeup.js
+++
@@ -1,5 +0,0 @@
-function changeup() {
-    const username_container = document.querySelector(".cupc .unc");
-    const old_password_container = document.querySelector(".cupc .opwc");
-    const new_password_container = document.querySelector(".cupc .npwc");
-}
