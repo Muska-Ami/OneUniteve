703a8148799b137e82dca3e9d57630d9b75fd5d5
XiaMoHuaHuo_CN <79651409+XiaMoHuaHuo-CN@users.noreply.github.com>
2022-07-28 08:20:25 +0800

    Create graph.php

--- api/graph.php
+++ api/graph.php
@@ -3,7 +3,7 @@ class Graph
 {
     public function startLoginUrl(): string
     {
-        return 'https://login.live.com/oauth20_authorize.srf?client_id=' . $this->getclientid() . '&scope=' . $this->getsocpe() . '&response_type=token&redirect_uri=' . $this->getredirecturi();
+        return 'https://login.live.com/oauth20_authorize.srf?client_id=' . $this->getclientid() . '&scope=' . $this->getscope() . '&response_type=token&redirect_uri=' . $this->getredirecturi();
     }
 
     private function getclientid(): string
@@ -11,9 +11,9 @@ class Graph
         return '1a2cb21b-c21a-4875-9652-260f42d46e3a';
     }
 
-    private function getsocpe(): string
+    private function getscope(): string
     {
-        return urlencode('https://graph.microsoft.com/Files.ReadWrite.All https://graph.microsoft.com/Sites.ReadWrite.All https://graph.microsoft.com/offline_access');
+        return urlencode('https://graph.microsoft.com/Files.ReadWrite.All');
     }
 
     private function getredirecturi(): string
